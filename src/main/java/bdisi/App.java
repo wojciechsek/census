73e8d6d92e4507800d64f9bc62f19754a6c134b7
tslipa <p.cegiela@outlook.com>
2021-01-09 14:39:55 +0100

    Attempts to make calling stored procedures work.
    So far not successful at all.

--- src/main/java/bdisi/App.java
+++ src/main/java/bdisi/App.java
@@ -1,13 +1,53 @@
 package bdisi;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import javax.persistence.*;
+import java.sql.PreparedStatement;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static SessionFactory getSessionFactory() {
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+        StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+        return configuration.buildSessionFactory(registryBuilder.build());
+    }
+
+    public static void main( String[] args ) {
+        Session session = App.getSessionFactory().openSession();
+
+        EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "censuspersistence" );
+
+        EntityManager entityManager = emfactory.createEntityManager( );
+
+        StoredProcedureQuery query = entityManager
+                .createStoredProcedureQuery("printPersonalData")
+                .registerStoredProcedureParameter("pesel", String.class,
+                        ParameterMode.IN)
+                .registerStoredProcedureParameter("result", String.class,
+                        ParameterMode.OUT)
+                .setParameter("pesel", "00291407177");
+
+        query.execute();
+
+
+        /*StoredProcedureQuery procedureQuery = session.createStoredProcedureQuery("displayCityStats");
+        procedureQuery.registerStoredProcedureParameter("pesel", String.class, ParameterMode.IN);
+        procedureQuery.registerStoredProcedureParameter("result", String.class, ParameterMode.OUT);
+        procedureQuery.setParameter("pesel", "00291407177");
+        procedureQuery.execute();
+
+        String dupa = (String) procedureQuery.getOutputParameterValue("result");*/
+        System.out.println("Hello World!");
     }
 }
